9b768b04f1b41d19b59bc56097f3e38cc4773719
Marc Riera <mrc2407@gmail.com>
2014-11-13 17:33:46 +0100

    4th day

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,4 +3,52 @@ class EntriesController < ApplicationController
     @project = Project.find params[:project_id]
     @entries = @project.entries
   end
+
+  def new
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.new
+  end
+
+  def create
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.new entry_params
+
+    if @entry.save
+      redirect_to action: :index, controller: :entries, project_id: @project.id
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.find params[:id]
+  end
+
+  def update
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.find params[:id]
+
+    if @entry.update_attributes entry_params
+      redirect_to action: :show, controller: :entries, project_id: @project.id, id: @entry.id
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project = Project.find params[:project_id]
+    @entry = @project.entries.find params[:id]
+    if @entry.destroy
+      redirect_to action: :index
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:hours, :minutes, :date)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  def flash_message
+    flash.keys.map do |key|
+      message_div(key)
+    end.join.html_safe
+  end
+
+  private
+
+  def message_div(type)
+    content_tag :div, class: 'message ' + type.to_s do
+      content_tag :p do
+        flash[type]
+      end
+    end
+  end
 end
