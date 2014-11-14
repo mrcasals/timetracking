fe5f9faed7f2fcaf9a912ce94b9ca34a14375ec9
Marc Riera <mrc2407@gmail.com>
2014-11-14 13:50:44 +0100

    Add projects to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :groups, through: :memberships
   has_many :memberships
+  has_many :projects, through: :groups
 end
