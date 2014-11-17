dc49f2aa9840cd1472866228a17ed589b4f531fe
Marc Riera <mrc2407@gmail.com>
2014-11-17 14:40:23 +0100

    Add specs for Project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -10,15 +10,28 @@ describe 'Project' do
       expect(project).to be_valid
     end
 
-    it 'is not valid without a name'
+    it 'is not valid without a name' do
+      project.name = nil
+      expect(project).not_to be_valid
+    end
     context 'when the name is longer than 30 chars' do
-      it 'is not valid'
+      it 'is not valid' do
+        project.name = 'This is a really really really looooong name to check if it fails'
+        expect(project).not_to be_valid
+      end
     end
     context 'when the name is already taken' do
-      it 'is not valid'
+      it 'is not valid' do
+        Project.create(name: 'Project 1')
+        project.name = 'Project 1'
+        expect(project).not_to be_valid
+      end
     end
     context 'when the name has special characters' do
-      it 'is not valid'
+      it 'is not valid' do
+        project.name = 'My project!'
+        expect(project).not_to be_valid
+      end
     end
   end
 end
